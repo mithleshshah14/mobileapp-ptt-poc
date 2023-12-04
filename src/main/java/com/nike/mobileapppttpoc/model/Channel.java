fe266c7086010c48f472a87fceeab39e10bf2ff3
Mithlesh Shah <mithleshshah84@gmail.com>
2023-12-04 12:51:06 +0530

    uuid update
    updated the uuid

--- src/main/java/com/nike/mobileapppttpoc/model/Channel.java
+++ src/main/java/com/nike/mobileapppttpoc/model/Channel.java
@@ -15,8 +15,8 @@ import org.hibernate.annotations.GenericGenerator;
 public class Channel {
 
   @Id
-  @GeneratedValue(generator = "system-uuid")
-  @GenericGenerator(name = "system-uuid", strategy = "uuid")
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
   private String uuid;
 
   private String channelName;
